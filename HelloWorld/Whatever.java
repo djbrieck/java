9961d031f69dc3f8e752964c9d2a5db77a1a265a
Daniel J Brieck Jr <djbrieck@gmail.com>
2025-06-17 14:55:29 -0700

    Updating the comment to include the full picture if class was changed to public then the file name would need to match

--- HelloWorld/Whatever.java
+++ HelloWorld/Whatever.java
@@ -1,5 +1,6 @@
 class HelloThere_wTea{
     // since this is default package, the name of the class is HelloThere_wTea and does not match the file name.
+    // Notice its is not public aka class public HelloThere_wTea then the file name must match the class name.
     public static void main (String[] args) {
         System.out.println("Hello There with Tea!");
     }
