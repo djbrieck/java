d4b330a775eef79af3f9c3c3cc014dac9a8731b1
Daniel J Brieck Jr <djbrieck@gmail.com>
2025-06-17 14:48:50 -0700

    Showing in this case class name does not have to match file name

--- HelloWorld/Hello.java
+++
@@ -1,6 +0,0 @@
-class HelloT{
-    // since this is default package, the name of the class is HelloT and does not match the file name.
-    public static void main (String[] args) {
-        System.out.println("Hello There with Tea!");
-    }
-}
---
+++ HelloWorld/Whatever.java
@@ -0,0 +1,6 @@
+class HelloThere_wTea{
+    // since this is default package, the name of the class is HelloThere_wTea and does not match the file name.
+    public static void main (String[] args) {
+        System.out.println("Hello There with Tea!");
+    }
+}
