2e67e8d8b6990f1a2bc7b03fcf34cde8e3999976
Daniel J Brieck Jr <djbrieck@gmail.com>
2025-06-18 14:10:27 -0700

    Adding primitives example

---
+++ Types/Primitives.java
@@ -0,0 +1,25 @@
+public class Primitives {
+
+    public static void main(String[] args) {
+        // Primitive types in Java
+        boolean isTrue = true; // boolean
+        char letter = 'A'; // char
+        byte smallNumber = 100; // byte
+        short mediumNumber = 10000; // short
+        int number = 100000; // int
+        long largeNumber = 10000000000L; // long
+        float decimalNumber = 10.5f; // float
+        double preciseDecimal = 20.99; // double
+
+        // Print the values
+        System.out.println("Boolean: " + isTrue);
+        System.out.println("Char: " + letter);
+        System.out.println("Byte: " + smallNumber);
+        System.out.println("Short: " + mediumNumber);
+        System.out.println("Int: " + number);
+        System.out.println("Long: " + largeNumber);
+        System.out.println("Float: " + decimalNumber);
+        System.out.println("Double: " + preciseDecimal);
+    }
+
+}
